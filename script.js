25e41a4007033cbe89e435bef7f269e2454340d5
lucasdsht <lucasdouchet17112003@gmail.com>
2022-09-15 09:46:46 +0200

    first exercise

---
+++ script.js
@@ -0,0 +1,16 @@
+/** Commentaire 
+ * sur plusieurs
+ * lignes 
+ */
+
+let number = 12;
+let firstname = "coucou";
+
+const coucou = 1
+
+const bool = false
+
+// demander son prenom a l'utilisateur & afficher sur la page
+let text = window.prompt('votre prenom ?')
+
+document.write(`tu t'apelle ${text}`)
