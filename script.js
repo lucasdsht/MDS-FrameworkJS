b58f1c148537b9f14e9de4b8ed7bdb329171b8ea
lucasdsht <lucasdouchet17112003@gmail.com>
2022-09-15 10:05:52 +0200

    feat: second exercise

--- script.js
+++ script.js
@@ -11,6 +11,15 @@ const coucou = 1
 const bool = false
 
 // demander son prenom a l'utilisateur & afficher sur la page
-let text = window.prompt('votre prenom ?')
+let text = prompt('votre prenom ?')
 
 document.write(`tu t'apelle ${text}`)
+
+// Créer un tableau qui contient 5 prenom
+const nameTable = [
+    'Lucas',
+    'Maxence',
+    'Rémi',
+    'Alix',
+    'Loïc',
+]
