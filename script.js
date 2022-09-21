3a3f96e9965266696df6b6fba47d5f0690fcdcd4
lucasdsht <lucasdouchet17112003@gmail.com>
2022-09-21 19:43:57 +0200

    feat: js comment & css butotn reset

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+// Récupération des éléments du DOM 
 const passwordInput = document.getElementById('passwordInput');
 const passwordButton = document.getElementById('passwordButton');
 const eyeIcon = document.getElementById('icon');
 const eyeIconSlash = document.getElementById('icon-slash');
 
+// Initialisation de l'évenement au click du bouton
 passwordButton.addEventListener('click', () => {
+
+    //! ternaire pour definir le type d'input a changer (liberté prise pour plus de simplicité)
     const type = passwordInput.getAttribute('type') === "password" ? "text" : "password"
+    
+    // changement du type
     passwordInput.setAttribute('type', type)
 
+    // condition pour l'affichage des icones
     if (passwordInput.getAttribute('type') === "password") {
         eyeIconSlash.classList.add('hidden')        
         eyeIcon.classList.remove('hidden')
@@ -15,6 +22,7 @@ passwordButton.addEventListener('click', () => {
         eyeIcon.classList.add('hidden')
     }
     
+    // lancement du timer si l'input est de type "text"
     if (passwordInput.getAttribute('type') === 'text') {
         setTimeout(() => {
             passwordInput.setAttribute('type', "password")
